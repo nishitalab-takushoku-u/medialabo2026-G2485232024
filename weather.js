a5e7a00452754b930a4685d2d91558bdb9d05465
Your Name <248523@st.takushoku-u.ac.jp>
2026-05-28 17:10:35 +0900

    プロジェクト

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-
 // 課題3-2 のプログラムはこの関数の中に記述すること
 function print(data) {
     
@@ -15,13 +14,16 @@ console.log("都市名:", data.name);
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
+    let old = document.querySelector("#result");
 
+    if (old) {
+        old.remove();
+    }
     let result = document.createElement("div");
 
-    // id="result" を設定
+
     result.id = "result";
 
-    // body の最後に追加
     document.body.appendChild(result);
 
 
@@ -56,19 +58,44 @@ function printDom(data) {
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
+let b = document.querySelector('#send');
+b.addEventListener('click', sendRequest);
+
+
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
+function sendRequest() {
+    let keyword = document.querySelector("#city").value;
+    let url ="https://www.nishita-lab.org/web-contents/jsons/openweather/" + keyword +".json";
 
+    axios.get(url)
+        .then(showResult)  
+        .catch(showError)   
+        .then(finish);      
+}
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
+function showResult(resp) {
 
+    let data = resp.data;
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    console.log(data);
+    printDom(data);
+}
 
 // 課題6-1: 通信エラーが発生した時の処理
-
+function showError(err) {
+    console.log(err);
+}
 
 // 課題6-1: 通信の最後にいつも実行する処理
-
+function finish() {
+    console.log('Ajax 通信が終わりました');
+}
 
 ////////////////////////////////////////
 // 以下はグルメのデータサンプル
